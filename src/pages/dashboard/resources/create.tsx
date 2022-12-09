640baaab2f516d7985cd1788ef63c652d364f097
Joshua Michael <joshua.kale.michael@gmail.com>
2022-12-09 16:43:49 -0600

    style remove button and add select

--- src/pages/dashboard/resources/create.tsx
+++ src/pages/dashboard/resources/create.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { trpc } from 'src/utils/trpc';
 import { CldUploadButton, CldImage } from 'next-cloudinary';
+import { FaWindowClose } from 'react-icons/fa';
 
 const Create = () => {
    const [mainImage, setMainImage] = useState('');
@@ -15,6 +16,7 @@ const Create = () => {
    const tags = trpc.tag.getAll.useQuery();
    const addResource = trpc.resource.add.useMutation();
    const deleteImage = trpc.cloudinary.deleteImage.useMutation();
+   console.log(tags.data);
 
    const router = useRouter();
 
@@ -39,7 +41,7 @@ const Create = () => {
    }
 
    return (
-      <main className='mx-auto max-w-4xl'>
+      <main className='mx-auto max-w-4xl p-3'>
          <h1 className='text-2xl'>Create New Resource</h1>
          <form className='space-y-3' onSubmit={handleOnSubmit}>
             <label htmlFor='name' className='flex flex-col'>
@@ -51,47 +53,62 @@ const Create = () => {
                   onChange={(e) => setTitle(e.target.value)}
                />
             </label>
+
             <label htmlFor='type' className='flex flex-col'>
                <span>Type</span>
-               <input
-                  type='text'
+               <select
                   name='type'
+                  id='type'
                   value={type}
                   onChange={(e) => setType(e.target.value)}
-               />
-            </label>
-            <div>
-               <CldUploadButton
-                  onUpload={(error: any, result: any, widget: any) => {
-                     setMainImage(result?.info?.public_id); // Updating local state with asset details
-                     setPreviewUrl(result?.info?.thumbnail_url);
-                     setCloudRes(result?.info);
-                     widget.close(); // Close widget immediately after successful upload
-                     console.log(error);
-                  }}
-                  uploadPreset='clipboard'
                >
-                  Upload to Cloudinary
-               </CldUploadButton>
-               {previewUrl != '' && (
-                  <>
-                     <CldImage
-                        src={previewUrl}
-                        width='400'
-                        height='400'
-                        alt='resource upload'
+                  <option value='ELA'>ELA</option>
+                  <option value='STEM'>STEM</option>
+               </select>
+            </label>
+            {!previewUrl && (
+               <div>
+                  <CldUploadButton
+                     onUpload={(error: any, result: any, widget: any) => {
+                        setMainImage(result?.info?.public_id); // Updating local state with asset details
+                        setPreviewUrl(result?.info?.thumbnail_url);
+                        setCloudRes(result?.info);
+                        widget.close(); // Close widget immediately after successful upload
+                        console.log(error);
+                     }}
+                     uploadPreset='clipboard'
+                  >
+                     Upload to Cloudinary
+                  </CldUploadButton>
+               </div>
+            )}
+            {previewUrl && (
+               <div className='relative inline-block'>
+                  <CldImage
+                     src={previewUrl}
+                     width='400'
+                     height='400'
+                     alt='resource upload'
+                  />
+                  <button
+                     className='absolute top-0 right-0'
+                     type='button'
+                     onClick={handleDeleteImage}
+                  >
+                     <FaWindowClose
+                        size={30}
+                        className='text-red-600 hover:text-red-500'
                      />
-                     <button type='button' onClick={handleDeleteImage}>
-                        remove
-                     </button>
-                  </>
-               )}
+                  </button>
+               </div>
+            )}
+            <div>
+               <input
+                  type='submit'
+                  value='Submit'
+                  className='cursor-pointer rounded-md bg-blue py-3 px-5 text-white'
+               />
             </div>
-            <input
-               type='submit'
-               value='Submit'
-               className='rounded-md bg-blue py-3 px-5 text-white'
-            />
          </form>
       </main>
    );
